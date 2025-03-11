3da758dad4e6808e3b5c2da558c62553317115b7
Mohamed Hamdy <eng.mohamed.hamdy@hotmail.com>
2025-03-12 00:59:57 +0200

    older solution used O(n) time and O(n) memory, which is incorrect for the question, new solution uses O(1) (constant time complexity) (N-M)

--- NeetCode/287.Find the Duplicate Number.py
+++ NeetCode/287.Find the Duplicate Number.py
@@ -1,8 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        hashMap = set()
-        for num in nums:
-            if num in hashMap:
-                return num
-            else:
-                hashMap.add(num)
+        slow,fast = 0,0
+        while True:
+            slow = nums[slow] # one hop
+            fast = nums[nums[fast]] # two hops
+
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            if slow2 == slow:
+                break
+            slow = nums[slow]
+            slow2 = nums[slow2]
+        return slow
